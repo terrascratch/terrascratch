ad0718aa45a52d994776dc1514afdec32f767cbb
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-08-30 23:06:43 -0300

    feat: add icons metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
   title: "Infra for Babies",
   description:
     "Easy-to-use dashboard where you can build your terraform code like playing with lego blocks.",
