1a0f3b835d282167d5ec1c2ba676a1201cb13691
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-09-10 17:12:45 -0300

    fix: remove useless mocks

--- mocks/containers.ts
+++ mocks/containers.ts
@@ -1,26 +1,5 @@
-import { ElementContainer, InfraElement } from "@/infra-elements/types";
-import { ec2, securityGroup, securityGroupRule, subnet, vpc } from "./infra-elements";
+import { ElementContainer } from "@/infra-elements/types";
 
-function makeContainer(element: InfraElement) {
-  return {
-    name: element.type,
-    element,
-    children: [],
-  };
-}
-
-export const containers: ElementContainer[] = [
-  makeContainer(securityGroupRule),
-  makeContainer(securityGroup),
-  makeContainer(vpc),
-  makeContainer(subnet),
-  makeContainer(ec2)
-];
-
-export function getRandomContainer() {
-  const index = Math.floor(Math.random() * containers.length);
-  return containers[index];
-}
 
 export const defaultElementContainer: ElementContainer = {
   name: "Main Subnet",
