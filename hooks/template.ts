0c78cf6e3fb13d3a0b83da5c41420c24cf7f65d9
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2023-12-04 18:22:40 -0300

    feat: add help links on resource creation

--- hooks/template.ts
+++ hooks/template.ts
@@ -1,5 +1,6 @@
 import templates from "@/infra-elements/templates/aws.json";
 import examples from "@/infra-elements/templates/examples/aws.json";
+import help from "@/infra-elements/templates/help.json";
 import { ElementTemplate } from "@/infra-elements/templates/type";
 
 export function useTemplate(elementType: string) {
@@ -30,4 +31,13 @@ export function useTemplate(elementType: string) {
 export function useExample(elementType: string) {
   const example = examples.find((example) => example.type === elementType);
   return example?.templates ?? [];
+}
+
+export function useHelp(elementTypes: string[]) {
+  return elementTypes.map(type => {
+    return {
+      type: type,
+      help: (help as Record<string, string>)[type]
+    }
+  })
 }
